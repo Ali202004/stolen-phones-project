4a68d9c87ccc9d84c24aa511ebac9cb7240d7cf8
Ali202004 <alanonymoa23@gmail.com>
2026-04-12 03:48:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # هنا Flask سيبحث تلقائياً داخل مجلد 'templates' عن الملف
     return render_template('index.html')
+
+@app.route('/add')
+def add_page():
+    return render_template('add.html') # تأكد أن هذا الملف موجود في مجلد templates
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
