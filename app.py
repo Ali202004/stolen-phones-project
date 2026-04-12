d59eb8c1df59f12567b41e210cb6b8bde4b4ed99
Ali202004 <alanonymoa23@gmail.com>
2026-04-12 03:13:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def home():
     return "موقع مشروع الهواتف المسروقة يعمل بنجاح!"
 
 if __name__ == '__main__':
-    # Render يحدد المنفذ تلقائياً عبر متغير بيئي اسمه PORT
+    # Render يحدد المنفذ تلقائياً، وإذا لم يجد، يستخدم 10000
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
