45fa8c3842bf1b0cee111aaf05a280838b3e2db1
Ali202004 <alanonymoa23@gmail.com>
2026-04-13 02:56:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import psycopg2
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 def get_db():
@@ -26,13 +27,7 @@ def add():
             conn.close()
             return "تم الحفظ بنجاح! <a href='/'>العودة</a>"
         except Exception as e:
-            # هنا سيظهر الخطأ الحقيقي في الـ Logs في Render
-            return f"خطأ في قاعدة البيانات: {str(e)}"
-    return render_template('add.html')
-
-if __name__ == "__main__":
-    app.run()
-        return "تم حفظ البلاغ بنجاح! <a href='/'>العودة للرئيسية</a>"
+            return f"خطأ: {str(e)}"
     return render_template('add.html')
 
 if __name__ == "__main__":
